34090aaf08d329b7a98feaf757c3a8afb3e58438
deehas <sahbdulsalam@gmail.com>
2023-12-10 21:36:33 +0100

    update: setup testing configuration

--- config.py
+++ config.py
@@ -6,8 +6,30 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Configuration(object):
+    DEBUG = False
+    TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfiguration(Configuration):
+    DEBUG = False
+    DEVELOPMENT = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "todo.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfiguration(Configuration):
+    DEBUG = True
+    TESTING = True
+    SERVER_NAME = "TESTSERVER.localdomain"
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    WTF_CSRF_ENABLED = False
+
+
+app_config = {
+    "test": TestingConfiguration,
+    "development": DevelopmentConfiguration,
+}
