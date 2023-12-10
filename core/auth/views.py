0041dbc9e249b3420b702b7a8a106290b49d2b53
deehas <sahbdulsalam@gmail.com>
2023-12-10 21:30:03 +0100

    update: add try except block to functions

--- core/auth/views.py
+++ core/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, current_user
 from . import auth
 from .forms import RegistrationForm, LoginForm
 from ..models.auth import User
@@ -9,40 +9,65 @@ from werkzeug.urls import urlsplit
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for("task.tasks"))
+    # if current_user.is_authenticated:
+    #     return redirect(url_for("home.homepage"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data.lower(), email=form.email.data.lower())
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash("Congratulations, you are now a registered user!")
-        return redirect(url_for("auth.login"))
+        try:
+            user = User(
+                username=form.username.data.lower(), email=form.email.data.lower()
+            )
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("Congratulations, you are now a registered user!", category="success")
+            return redirect(url_for("auth.login"))
+
+        except Exception as e:
+            flash("Registration failed, please try again.", category="danger")
+            db.session.rollback()
+
+    elif form.errors:
+        flash(form.errors, category="danger")
+
     return render_template("register.html", title="Register", form=form)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    nologin = False
     if current_user.is_authenticated:
-        return redirect(url_for("task.tasks"))
+        return redirect(url_for("home.homepage"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data.lower()).first()
-        if user is None or not user.check_password(form.password.data):
-            nologin = True
-        else:
-            # If next page parameter exist in url, redirect to the url
-            # this means the user was redirected to the login page from the original route
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get("next")
-            if not next_page or urlsplit(next_page).netloc != "":
-                # else redirect to the task management page
-                next_page = url_for("task.tasks")
-            return redirect(next_page)
+        try:
+            user = User.query.filter_by(email=form.email.data.lower()).first()
+            if user is None or not user.check_password(form.password.data):
+                flash(
+                    "Login failed. Invalid email or password. Please try again.",
+                    category="danger",
+                )
+            else:
+                # If next page parameter exist in url, redirect to the url
+                # this means the user was redirected to the login page from the original route
+                login_user(user, remember=form.remember_me.data)
+                next_page = request.args.get("next")
+                if not next_page or urlsplit(next_page).netloc != "":
+                    # else redirect to the home page
+                    next_page = url_for("home.homepage")
 
-    return render_template("login.html", title="Sign In", form=form, message=nologin)
+                flash(
+                    "Welcome back! You have successfully logged in.", category="success"
+                )
+                return redirect(next_page)
+
+        except Exception as e:
+            flash("Login failed, please try again.", category="danger")
+            db.session.rollback()
+            
+    elif form.errors:
+        flash(form.errors, category="danger")
+
+    return render_template("login.html", title="Sign In", form=form)
 
 
 @auth.route("/logout")
--- core/home/views.py
+++ core/home/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash
 from flask_login import login_required, current_user
 from ..models.todo import Todo
 from ..models.todo import Category
@@ -9,35 +9,48 @@ from datetime import datetime
 @home.route("/home")
 @login_required
 def homepage():
-    user = current_user
-    # Query todo table for todos linked to logged in user
-    todo = Todo.query.filter_by(author=user)
-    # Get current Date and Time
-    date = datetime.now()
-    # Format datetime to String
-    now = date.strftime("%Y-%m-%d")
+    no_of_Task_ID_Business = 0
+    no_of_Task_ID_Personal = 0
+    no_of_Task_ID_Other = 0
+    try:
+        user = current_user
+        # Query todo table for todos linked to logged in user
+        todo = Todo.query.filter_by(author=user).order_by(Todo.date)
+        # Get current Date and Time
+        date = datetime.now()
+        # Format date to String
+        now = date.strftime("%Y-%m-%d")
+        # Format time to String
+        currentTime = date.strftime("%H:%M")
 
-    category1 = Category.query.get(1)
-    category2 = Category.query.get(2)
-    category3 = Category.query.get(3)
+        # Retrieve categories with their IDs
+        category1 = Category.query.get(1)
+        category2 = Category.query.get(2)
+        category3 = Category.query.get(3)
 
-    Task_ID_Business = Todo.query.filter_by(category="Business", author=user)
-    Task_ID_Personal = Todo.query.filter_by(category="Personal", author=user)
-    Task_ID_Other = Todo.query.filter_by(category="Other", author=user)
+        # Get each task under each category
+        Task_ID_Business = Todo.query.filter_by(category=category1.name, author=user)
+        Task_ID_Personal = Todo.query.filter_by(category=category2.name, author=user)
+        Task_ID_Other = Todo.query.filter_by(category=category3.name, author=user)
 
-    no_of_Task_ID_Business = Task_ID_Business.count()
-    no_of_Task_ID_Personal = Task_ID_Personal.count()
-    no_of_Task_ID_Other = Task_ID_Other.count()
+        # Get the number of task under each category
+        no_of_Task_ID_Business = Task_ID_Business.count()
+        no_of_Task_ID_Personal = Task_ID_Personal.count()
+        no_of_Task_ID_Other = Task_ID_Other.count()
+
+    except Exception as e:
+        flash("Loading homepage failed, please try again.", category="danger")
 
     return render_template(
         "home.html",
         title="Home Page",
         todo=todo,
         no_of_business_tasks=no_of_Task_ID_Business,
-        no_of_other_tasks=no_of_Task_ID_Other,
         no_of_personal_tasks=no_of_Task_ID_Personal,
+        no_of_other_tasks=no_of_Task_ID_Other,
         category_i=category1,
         category_ii=category2,
         category_iii=category3,
         DateNow=now,
+        TimeNow=currentTime,
     )
