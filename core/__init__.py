95a7cca8b3d868852220559e3edc9fad01fa4ffe
deehas <sahbdulsalam@gmail.com>
2024-01-13 05:37:10 +0100

    feat: add jwt authentication

--- core/__init__.py
+++ core/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from core.models.todo import db
 from core.task import views
 from flask_login import LoginManager
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
@@ -14,9 +16,11 @@ login = LoginManager()
 def create_app():
     # create and configure the app
     app.config.from_object(app_config[os.environ.get("APP_SETTINGS")])
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     db.init_app(app)
     login.init_app(app)
     migrate = Migrate(app, db)
+    jwt = JWTManager(app)
     login.login_view = "auth.login"
 
     from core import routes
@@ -26,8 +30,8 @@ def create_app():
     from .home import home as home_blueprint
     from .auth import auth as auth_blueprint
 
-    app.register_blueprint(task_blueprint)
-    app.register_blueprint(home_blueprint)
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(task_blueprint, url_prefix="/task")
+    app.register_blueprint(home_blueprint, url_prefix="/home")
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     return app
